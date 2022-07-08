7a5f47957bd2b924664df44a6c10abcac5c5895d
haneesha25 <85246258+haneesha25@users.noreply.github.com>
2022-07-07 23:44:40 -0400

    Update HomeVC.swift

--- FlowerApp/FlowerApp/ViewControllers/HomeVC.swift
+++ FlowerApp/FlowerApp/ViewControllers/HomeVC.swift
@@ -28,10 +28,7 @@ class HomeVC: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        self.navigationController?.navigationBar.isHidden = false
-    }
+    
 
 }
 
